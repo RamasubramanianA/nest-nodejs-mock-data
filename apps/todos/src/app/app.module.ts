401fb8c8b1300fa06c3cfa179a2a4380c0ea27f6
RamasubramanianA <ram24subbu4@gmail.com>
2022-09-25 13:32:05 +0530

    lowdb added

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosService } from './todos/todos.service';
+import { AuthModule } from '@enbook/auth';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService, TodosService],
 })
-export class AppModule {}
+export class AppModule { }
